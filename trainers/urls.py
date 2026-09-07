218b274ae76f78616c7e651a0edd421bbab2be9e
辰 <3294502647@qq.com>
2026-09-07 21:08:33 +0800

    feat:退出登录按钮

--- trainers/urls.py
+++ trainers/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from . import views
@@ -6,5 +7,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.login_view, name="login"),
     path("register/", views.register_view, name="register"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
     path("new-game/", views.new_game, name="new_game"),
 ]
